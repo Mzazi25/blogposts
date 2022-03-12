6a941114cb5c7216c27fb7af90a7120ebb0d4239
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-03-12 11:39:37 +0300

    Updated configs and added destinations for uploaded photos

--- config.py
+++ config.py
@@ -6,13 +6,19 @@ class Config:
     SECRET_KEY= os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:Mzazi25@localhost/blog'
-    QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    BLOG_API_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  # or other relevant config var
-    # if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-    #     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") 
 
 class DevConfig(Config):
 
