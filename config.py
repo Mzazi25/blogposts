cc3dc0bc1b9fec42f6f17c4325e567624c9271c1
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-03-12 10:01:58 +0300

    Updated configs

---
+++ config.py
@@ -0,0 +1,23 @@
+import  os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY= os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user:Mzazi25@localhost/blog'
+    QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+
+class ProdConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  # or other relevant config var
+    # if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    #     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
+class DevConfig(Config):
+
+    DEBUG = True
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
