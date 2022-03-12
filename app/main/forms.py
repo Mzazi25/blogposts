e862e707ef872f14e7826f4b526a04b755a5385a
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-03-12 14:05:34 +0300

    Added comment Form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField,
 from wtforms.validators import InputRequired,Email,EqualTo
 from ..models import User
 from wtforms import ValidationError
 from wtforms import StringField,PasswordField,BooleanField,SubmitField,TextAreaField
+from wtforms.widgets import TextArea
 
 
 class UpdateProfile(FlaskForm):
@@ -12,3 +13,5 @@ class UpdateProfile(FlaskForm):
 class BlogForm(FlaskForm):
    message = StringField('message', widget=TextArea(), validators=[InputRequired()])
    title = StringField('Blog Title',widget=TextArea(),validators=[InputRequired()])
+class CommentForm(FlaskForm):
+   comment = StringField('comment', widget=TextArea(), validators=[InputRequired()])
